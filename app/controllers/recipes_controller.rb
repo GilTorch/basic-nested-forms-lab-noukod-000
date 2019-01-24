ab6676975d4bd7a265aed220f50600cf4d261c5b
Gilbert Torchon <gilbert@noukod.com>
2019-01-24 18:45:04 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+
   end
 
   def index
@@ -9,6 +10,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(title:'')
+    @recipe.ingredients.build(title:'')
   end
 
   def create
